6f6b9e593c5d7a7d5c954ef42e1f190e92799f9a
lulalachen <lulalachen@lulalachensMBP3.Home>
2015-12-20 04:15:52 +0800

    modify gitupload

--- gitUpload.sh
+++ gitUpload.sh
@@ -4,5 +4,14 @@ if [[ $1 ]]; then
   commit_message="$1"
   git add .
   git commit -m "$commit_message"
-  git push
+  # git push origin master
+else
+  while [ "${commit_message}" == "" ]
+  do
+    read -p "Please enter your commit message: " commit_message
+  done
+  echo 'hhw'
+  git add .
+  git commit -m "$commit_message"
+  # git push origin master
 fi
