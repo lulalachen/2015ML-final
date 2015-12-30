94f67d572c59e0de4820a6bad435f678365d6da2
lulalachen <lulalachen@lulalachensMBP3.Home>
2015-12-30 13:59:10 +0800

    course stats

--- wowLulala/combine_sample_train_x_with_y.js
+++ wowLulala/combine_sample_train_x_with_y.js
@@ -5,21 +5,41 @@ var fsp = require('fs-time-prefix'),
     util = require('util'),
     cli = require('cli-color');
 
-fsp.readFile('../data/sample_train_x.csv')
+var dataToBeGenerate = process.env.DATATYPE || 'train';
+var target;
+if (dataToBeGenerate === 'train')
+  target = '../data/sample_train_x.csv';
+else if (dataToBeGenerate === 'test')
+  target = '../data/sample_test_x.csv';
+
+fsp.readFile(target)
 .then(function(chunck){
   // Convert into row-based dataset //
   var rows = chunck.toString().split('\n');
   var return_data = [];
   rows.forEach(function(row){
     var columns = row.split(',');
-    return_data.push(columns);
+    var temp = [];
+    columns.forEach(function(col){
+      if (Number(col).toString() === 'NaN')
+        temp.push(col);
+      else
+        temp.push(Number(col));
+    });
+    return_data.push(temp);
   });
-  // console.log('Finish row-based transformation.');
-  return readTruthTrain(return_data, '../data/truth_train.csv');
+  return_data.pop(); //Remove empty last line
+  if (dataToBeGenerate === 'test'){
+    fsp.writeJsonFile('../data/sample_test_x.json', return_data);
+    return null
+  }else
+    return readTruthTrain(return_data, '../data/truth_train.csv');
 })
 .then(function(data){
-  fsp.writeJsonFile('../data/sample_train_x_with_y.json', data);
-  console.log(cli.cyan("sample_train_x_with_y.json") + ' is generated.' );
+  if (data !== null){
+    fsp.writeJsonFile('../data/sample_train_x_with_y.json', data);
+    console.log(cli.cyan("sample_train_x_with_y.json") + ' is generated.' );
+  }
 })
 
 function readTruthTrain (return_data,path){
@@ -28,8 +48,8 @@ function readTruthTrain (return_data,path){
       .then(function(chunck){
         var rows = chunck.toString().split('\n');
         return_data[0].push('y');
-        for (var i = 1; i < return_data.length - 1 ; i++) {
-          return_data[i].push(rows[i-1].split(',')[1]);
+        for (var i = 1; i < return_data.length ; i++) {
+          return_data[i].push(Number(rows[i-1].split(',')[1]));
         };
         for (var i = 0; i < return_data.length; i++) {
           if (return_data[i].length === 0)
--- wowLulala/logisticRgression.js
+++ wowLulala/logisticRgression.js
@@ -4,102 +4,121 @@ var Promise = require('bluebird');
 var stats = require("stats-lite");
 
 
-fsp.readFile('../data/enrollment_train_with_course_stats\(y\).json')
+fsp.readFile('../data/sample_train_x_with_course_stats.json')
 .then(function(chunk){
   var data = JSON.parse(chunk);
   data.shift(); // Remove title
-  // data.pop(); // Remove bottom empty array
   var y = [];
-  var a=0,b = 0;
   data.forEach(function(val){
-    y.push(val.pop());
+    y.push([val.pop()]);
   });
   return [data, y];
 })
-.then(function (chunk) {
-  // Conver to numbers....................
-  var x = chunk[0];
-  var y = chunk[1];
-  for (var i = 0; i < x.length; i++){
-    for (var j = 0; j < x[i].length; j++){
-      x[i][j] = Number(x[i][j]);
-    }
-    y[i] = [Number(y[i])];
-  }
-  // console.log('herer');
-  return [x,y];
-})
-.spread(function(data, y){
-  // ...........Normalize........... //
+// .spread(function(data, y){
+//   // ...........Normalize........... //
 
-  // Convert into column-based dataset //
-  var normalizedData = [];
-  for (var i = 0; i < data[0].length; i++) {
-    normalizedData[i] = [];
-  };
+//   // Convert into column-based dataset //
+//   var normalizedData = [];
+//   for (var i = 0; i < data[0].length; i++) {
+//     normalizedData[i] = [];
+//   };
 
-  data.forEach(function(row){
-    for (var i = 0; i < row.length; i++) {
-      // console.log(i);
-      normalizedData[i].push(row[i]);
-    };
-  });
-  normalizedData.forEach(function(col, col_id, arr_col){
-    var mean      = stats.mean(col);
-    var variance  = stats.stdev(col);
-    col.forEach(function(each, idx, arr){
-      if (variance !== 0){
-        // course = 1 ; course_info = 2 //
-        arr[idx]-=mean;
-        arr[idx]/=variance;
-      } else {
-        arr[idx] = 0;
-      }
-    });
-  });
-  return [normalizedData, y];
-})
-.spread(function(normalizedData, y){
-  var rowBasedData = [];
-  for (var i = 0; i < normalizedData[0].length; i++) {
-    rowBasedData[i] = [];
-  };
-  normalizedData.forEach(function(col, idx, arr){
-    // console.log(idx +' ' + col.length);
-    for (var i = 0; i < col.length; i++) {
-      rowBasedData[i].push(col[i]);
-    };
-  });
-  return [rowBasedData, y];
-})
+//   data.forEach(function(row){
+//     for (var i = 0; i < row.length; i++) {
+//       // console.log(i);
+//       normalizedData[i].push(row[i]);
+//     };
+//   });
+//   normalizedData.forEach(function(col, col_id, arr_col){
+//     var mean      = stats.mean(col);
+//     var variance  = stats.stdev(col);
+//     col.forEach(function(each, idx, arr){
+//       if (variance !== 0){
+//         // course = 1 ; course_info = 2 //
+//         arr[idx]-=mean;
+//         arr[idx]/=variance;
+//       } else {
+//         arr[idx] = 0;
+//       }
+//     });
+//   });
+//   return [normalizedData, y];
+// })
+// .spread(function(normalizedData, y){
+//   var rowBasedData = [];
+//   for (var i = 0; i < normalizedData[0].length; i++) {
+//     rowBasedData[i] = [];
+//   };
+//   normalizedData.forEach(function(col, idx, arr){
+//     // console.log(idx +' ' + col.length);
+//     for (var i = 0; i < col.length; i++) {
+//       rowBasedData[i].push(col[i]);
+//     };
+//   });
+//   return [rowBasedData, y];
+// })
 .spread(function(x,y){
-
-  var classifier = new ml.LogisticRegression({
-      'input' : x,
-      'label' : y,
-      'n_in' : 15,
-      'n_out' : 1
+  var knn = new ml.KNN({
+      data : x,
+      result : y
   });
+  // var svm = new ml.SVM({
+  //     x : x,
+  //     y : y
+  // });
 
-  classifier.set('log level',2);
+  // svm.train({
+  //     C : 1.0, // default : 1.0. C in SVM.
+  //     tol : 1e-4, // default : 1e-4. Higher tolerance --> Higher precision
+  //     max_passes : 20, // default : 20. Higher max_passes --> Higher precision
+  //     alpha_tol : 1e-5, // default : 1e-5. Higher alpha_tolerance --> Higher precision
 
-  var training_epochs = 800, lr = 0.01;
-  console.time('start_training');
-  classifier.train({
-      'lr' : lr,
-      'epochs' : training_epochs
-  });
-  console.timeEnd('start_training');
+  //     kernel : {type:"linear"}
+  //     // default : {type : "gaussian", sigma : 1.0}
+  //     // {type : "gaussian", sigma : 0.5}
+  //     // {type : "linear"} // x*y
+  //     // {type : "polynomial", c : 1, d : 8} // (x*y + c)^d
+  //     // Or you can use your own kernel.
+  //     // kernel : function(vecx,vecy) { return dot(vecx,vecy);}
+  // });
+  // var classifier = new ml.LogisticRegression({
+  //     'input' : x,
+  //     'label' : y,
+  //     'n_in' : 30,
+  //     'n_out' : 1
+  // });
 
-  console.log("Entropy : "+classifier.getReconstructionCrossEntropy());
-  fsp.writeFile('../results/classifier.js', classifier.toString());
-  return readAnother(classifier,'./data/enrollment_test_with_course_stats.json');
+  // classifier.set('log level',2);
+
+  // var training_epochs = 200, lr = 0.6;
+  // console.time('start_training');
+  // classifier.train({
+  //     'lr' : lr,
+  //     'epochs' : training_epochs
+  // });
+  // console.timeEnd('start_training');
+
+  // console.log(Function.keys(classifier));
+  console.log(Object.keys(knn));
+  fsp.writeJsonFile('./results/classifierKNN.js', knn);
+  return readAnother(knn,'../data/sample_test_x_with_course_stats.json');
 })
-.then(function(chunk){
-  var classifier = chunk[0];
-  var test_data = chunk[1];
-  var result = classifier.predict(test_data);
-  fsp.writeJsonFile('./results/logisticRegression.json', result);
+.spread(function(classifier, test_data){
+  // var result = classifier.predict(test_data);
+  var knn_result = [];
+  test_data.forEach(function(row, idx, arr){
+    var prediction = classifier.predict({
+      x : row,
+      k : 3,
+      // weightf : {type : 'gaussian', sigma : 10.0},
+      distance : {type : 'euclidean'}
+    });
+    if (idx % 1000 === 0)
+      console.log('Meow ' + idx);
+    knn_result.push(prediction);
+  })
+  // fsp.writeJsonFile('./results/svm.json', result);
+  fsp.writeJsonFile('./results/knn.json', knn_result);
   console.log('FUCKINNNNNG DONE!!!!');
 })
 .catch(function (err) {
