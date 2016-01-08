7f28e48c67dfb3b90e1a064a3eefad84b46b0728
lulalachen <lulalachen@lulalachensMBP3.Home>
2016-01-09 01:06:33 +0800

    adjusted course stats

--- wowLulala/course_stats.js
+++ wowLulala/course_stats.js
@@ -15,8 +15,8 @@ fsp.readFile('./results/course.json')
                 'about',
                 'chapter',
                 'combinedopenended',
-                'course',
-                'course_info',
+                // 'course',
+                // 'course_info',
                 'discussion',
                 'html',
                 'outlink',
@@ -25,18 +25,46 @@ fsp.readFile('./results/course.json')
                 'sequential',
                 'static_tab',
                 'vertical',
-                'video',
-                'dictation'
+                'video'
+                // 'dictation'
               ];
 
   courses.forEach(function(each, idx, arr){
     var temp = {};
     var category = [];
+    var coursePeriod = [];
+    var timePeriod = [86400 * 30 * 1000, 86400 * 40 * 1000];
+
     for (var i = 0; i < fields.length; i++) {
-      temp[fields[i]] = 0;
+      temp[fields[i]+'_30'] = 0;
+      temp[fields[i]+'_40'] = 0;
     };
     for (var i = 0; i < each.modules.length; i++) {
-        temp[each.modules[i].category] ++;
+      if (each.modules[i].category === 'course'){
+        var tempStart = (new Date(each.modules[i].courseStart)).getTime()
+        coursePeriod.push(tempStart);
+        coursePeriod.push(tempStart + timePeriod[0]);
+        coursePeriod.push(tempStart + timePeriod[1]);
+        break;
+      }
+    };
+
+    // console.log(coursePeriod);
+
+    for (var i = 0; i < each.modules.length; i++) {
+        if (temp[each.modules[i].category+'_30'] !== undefined || temp[each.modules[i].category+'_40'] !== undefined){
+          // Filter by target fields
+          switch (inWhichPeriod(each.modules[i].courseStart, coursePeriod)) {
+            case 'first':
+              temp[each.modules[i].category + '_30'] ++;
+              break;
+            case 'second':
+              temp[each.modules[i].category + '_40'] ++;
+              break;
+            default :
+              break;
+          }
+        }
     };
     counts.push({
       id : each.courseIdRaw,
@@ -44,20 +72,32 @@ fsp.readFile('./results/course.json')
     });
   })
 
-  fsp.writeJsonFile('./results/course_stats.json',counts);
+  console.log(counts[0]);
+  fsp.writeJsonFile('./results/course_stats_filter.json',counts);
 
   return counts;
 })
 .then(function(data){
-  for (var i = 0; i < data.length; i++) {
-    var stats = data[i].stats;
-    for (var j = 0; j < Object.keys(stats).length; j++) {
-      var field = Object.keys(data[i].stats)[j];
+  var target = ('train' === 'train')
+              ? ['../sample_train_x.csv', '../sample_train_x_with_filter_course_stat.csv']
+              : ['../sample_test_x.csv', '../sample_test_x_with_filter_course_stat.csv'];
+  var file = fsp.createWriteStream(target);
 
-    };
-  };
 
 })
+
+function inWhichPeriod(time, period){
+  var time = (time !== 'null') ? (new Date(time)).getTime() : (new Date(0)).getTime();
+  var seg = [time - period[0],period[1] - time]
+  // console.log(seg);
+  if ((time - period[0]) >= 0 && (period[1] - time) >= 0) {
+    return 'first';
+  } else if ((time - period[1]) >= 0 && (period[2] - time) >= 0 ){
+    return 'second';
+  } else {
+    return 'none';
+  }
+}
 
 
 function stats(data){
